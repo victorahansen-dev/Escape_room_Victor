b95d4bdf1605bebf91f4f21c74e859878e35d2c2
victorahansen-dev <victorahansen5gmail.com>
2026-05-12 19:28:23 +0200

    kan nå snu rundt til dør i rommet

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function switchRoom(newImage, klasse) {
-    document.querySelector(klasse).style.display = "none";
+    //document.querySelector(klasse).style.display = "none";
     document.body.style.backgroundImage = "url('" + newImage + "')";
 }
